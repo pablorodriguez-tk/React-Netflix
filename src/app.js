d82460fc5322cb7733e5530620a2192afee5f2d5
pablorodriguez-tk <pablorodriguez88@gmail.com>
2021-08-24 01:55:18 -0300

    add jumbotron component

--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
 import Jumbotron from './components/jumbotron';
 import JumboData from './fixtures/jumbo.json';
 
-const App = () => {
-  return (
-    <Jumbotron.Container>
-      {JumboData.map((item) => (
-        <Jumbotron key={item.id} direction={item.direction}>
-          <p>{item.title}</p>
-          <p>{item.subTitle}</p>
-          <p>{item.image}</p>
-          <p>{item.alt}</p>
-        </Jumbotron>
-      ))}
-    </Jumbotron.Container>
-  );
-};
+const App = () => (
+  <Jumbotron.Container>
+    {JumboData.map((item) => (
+      <Jumbotron key={item.id} direction={item.direction}>
+        <Jumbotron.Pane>
+          <Jumbotron.Title>{item.title}</Jumbotron.Title>
+          <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+        </Jumbotron.Pane>
+        <Jumbotron.Pane>
+          <Jumbotron.Image src={item.image} alt={item.alt} />
+        </Jumbotron.Pane>
+      </Jumbotron>
+    ))}
+  </Jumbotron.Container>
+);
 
 export default App;
