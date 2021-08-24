b66a478db10fd5588e71322b3083fcf65fc7a73a
pablorodriguez-tk <pablorodriguez88@gmail.com>
2021-08-24 04:25:06 -0300

    Add accordion

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
+import { FaqsContainer } from './containers/faqs';
 import FooterContainer from './containers/footer';
 import JumbotronContainer from './containers/jumbotron';
 
 const App = () => (
   <>
     <JumbotronContainer />
+    <FaqsContainer />
     <FooterContainer />
   </>
 );
